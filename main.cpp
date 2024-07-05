f840ce02228d3b12c437cfa7ff76a72d4be6f283
ua <anyugonna@gmail.com>
2024-07-05 07:40:09 -0500

    Revert "extern testing"
    This reverts commit 387cba5089b52345bde71faa83d0b9bbaf5a5fe1.

--- main.cpp
+++ main.cpp
@@ -3,17 +3,9 @@
 #include <string>
 #include <pqxx/pqxx>
 
-#include "extern.h"
-
 using namespace std;
-using namespace pqxx;
-
-void printNum();
 
 int main() {
-
-	num = 5;
-
 	fstream inputFile;
 	string password;
 
@@ -26,7 +18,7 @@ int main() {
 
 	try {
 		// Connect to the database
-		connection c("host=aws-0-us-west-1.pooler.supabase.com port=6543 dbname=postgres user=postgres.pwdgipuzwbjtpswldglm password=" + password);
+		pqxx::connection c("host=aws-0-us-west-1.pooler.supabase.com port=6543 dbname=postgres user=postgres.pwdgipuzwbjtpswldglm password=" + password);
 		if (c.is_open()) {
 			cout << "Opened database successfully: " << c.dbname() << endl;
 		}
@@ -35,25 +27,11 @@ int main() {
 			return 1;
 		}
 
-		work w(c);
-
-		result row = w.exec(
-			"SELECT title FROM books WHERE book_id=3"
-		);
-
-		w.commit();
-
-		cout << row[0][0] << endl;
-
 	}
 	catch (const exception& e) {
 		cerr << e.what() << endl;
 		return 1;
 	}
-
-	cout << "This was printed in main.cpp: " + to_string(num) << endl;
-	printNum();
-
 
 	return 0;
 }
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#inlucde "extern.h"
-
-using namespace std;
-
-void printNum() {
-	cout << "This was called in test.cpp: " + to_string(num) << endl;
-}
