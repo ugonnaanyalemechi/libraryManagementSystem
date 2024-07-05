6c9ab5bc333fe17b3915d9201447ad5702dcaddb
ua <anyugonna@gmail.com>
2024-07-04 20:18:50 -0500

    reposition file code to outside try and catch

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 
 int main() {
+	fstream inputFile;
+	string password;
+
+	inputFile.open("../../../../passwordDB.txt");
+
+	if (inputFile.fail())
+		cout << "Error has occurred in retrieving the file." << endl;
+
+	getline(inputFile, password);
+
 	try {
-		fstream inputFile;
-		string password;
-
-		inputFile.open("../../../../passwordDB.txt");
-
-		if (inputFile.fail())
-			cout << "Error has occurred in retrieving the file." << endl;
-
-		getline(inputFile, password);
-
 		// Connect to the database
 		pqxx::connection c("host=aws-0-us-west-1.pooler.supabase.com port=6543 dbname=postgres user=postgres.pwdgipuzwbjtpswldglm password=" + password);
 		if (c.is_open()) {
